661aae73907dab5342e4da3372827814653b3d55
Geza Csorba <csrbgeza@gmail.com>
2021-12-27 09:03:55 +0100

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,29 @@ import { AppComponent } from './app.component';
 import { PuzzlesComponent } from './puzzles/puzzles.component';
 import { ChessboardComponent } from './chessboard/chessboard.component';
 import { NgxChessgroundComponent, NgxChessgroundModule } from 'ngx-chessground';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NewsComponent } from './news/news.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { EventcalendarComponent } from './eventcalendar/eventcalendar.component';
+import { ToolbarmenuComponent } from './toolbarmenu/toolbarmenu.component';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [
     AppComponent, 
-    PuzzlesComponent, ChessboardComponent
+    PuzzlesComponent, ChessboardComponent, NewsComponent, 
+    GalleryComponent, AboutusComponent, EventcalendarComponent, 
+    ToolbarmenuComponent, 
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    NgxChessgroundModule
+    NgxChessgroundModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
